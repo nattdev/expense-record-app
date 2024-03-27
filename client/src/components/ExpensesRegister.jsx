a0f9d233c3a721829e656a3c5d8eda71548353ff
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-27 18:47:59 -0500

    Add category property to expense

--- client/src/components/ExpensesRegister.jsx
+++ client/src/components/ExpensesRegister.jsx
@@ -7,18 +7,22 @@ function ExpensesRegister() {
     const storedItems = JSON.parse(localStorage.getItem('expenses'));
 
     const initialExpenses = [
-        { id: 1, currency: "soles", "amount": 200 },
-        { id: 2, currency: "soles", "amount": 20.02 },
-        { id: 3, currency: "dolares", "amount": 4.01 },
-        { id: 4, currency: "soles", "amount": 5.20 },
-        { id: 5, currency: "soles", "amount": 1002.20 },
-        { id: 6, currency: "dolares", "amount": 10 },
-        { id: 7, currency: "soles", "amount": 200 },
-        { id: 8, currency: "soles", "amount": 20.02 }
+        { id: 1, currency: "soles", category: "alimentacion", "amount": 200 },
+        { id: 2, currency: "soles", category: "compras", "amount": 20.02 },
+        { id: 3, currency: "soles", category: "pasajes", "amount": 4.01 },
+        { id: 4, currency: "soles", category: "", "amount": 5.20 },
+        { id: 5, currency: "soles", category: "", "amount": 1002.20 },
+        { id: 6, currency: "soles", category: "", "amount": 10 },
+        { id: 7, currency: "soles", category: "", "amount": 200 },
+        { id: 8, currency: "soles", category: "", "amount": 20.02 }
     ]
 
     const [numberId, setNumberId] = useState(9);
-    const {expenses, setExpenses} = useExpenses();
+    const [isDetailShown, setIsDetailShown] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedId, setSelectedId] = useState("");
+
+    const { expenses, setExpenses } = useExpenses();
 
     const refAmount = useRef();
     const refCurrency = useRef();
@@ -37,7 +41,35 @@ function ExpensesRegister() {
         localStorage.setItem("expenses", JSON.stringify(expenses));
     }, [expenses]);
 
-    const expensesList = expenses.map((expense) => <li key={expense.id}><span>{expense.currency == "soles" ? "S/." : "$"}</span>{expense.amount}<button onClick={() => handleDeleteExpense(expense.id)}>X</button></li>);
+    const expenseDetail = (
+        <div id="expense-detail-wrapper">
+            <header>
+                <p>Categoría:</p><button onClick={handleCloseDetail}>X</button>
+            </header>
+            <ul>
+                <li>
+                    <input id="alimentacion" type="radio" name="category" value="alimentacion" onChange={handleCategoryChange} checked={selectedCategory === "alimentacion"}>
+                    </input>
+                    <label htmlFor="alimentacion">Alimentación
+                    </label>
+                </li>
+                <li>
+                    <input id="pasajes" type="radio" name="category" value="pasajes" onChange={handleCategoryChange} checked={selectedCategory === "pasajes"}>
+                    </input>
+                    <label htmlFor="pasajes">Pasajes
+                    </label>
+                </li>
+                <li>
+                    <input id="Compras" type="radio" name="category" value="compras" onChange={handleCategoryChange} checked={selectedCategory === "compras"}>
+                    </input>
+                    <label htmlFor="Compras">Compras
+                    </label>
+                </li>
+            </ul>
+        </div>
+    );
+
+    const expensesList = expenses.map((expense) => <li key={expense.id}><div onClick={() => handleExpenseDetail(expense.id)}><span>{expense.currency == "soles" ? "S/." : "$"}</span>{expense.amount}<p>{expense.category}</p></div><button onClick={() => handleDeleteExpense(expense.id)}>X</button></li>);
 
     function handleSubmitExpense(event) {
         event.preventDefault();
@@ -52,8 +84,30 @@ function ExpensesRegister() {
     }
 
     function handleDeleteExpense(id) {
-        console.log(id);
         setExpenses(expenses.filter(expense => expense.id !== id));
+    }
+
+    function handleExpenseDetail(id) {
+        setSelectedId(id);
+        setIsDetailShown(true);
+        setSelectedCategory((expenses.find(expense => expense.id == id)).category);
+    }
+
+    function handleCloseDetail() {
+        setIsDetailShown(false);
+        setExpenses(expenses.map(expense => {
+            if (expense.id === selectedId) {
+                return {
+                    ...expense,
+                    ["category"]: selectedCategory
+                };
+            }
+            return expense;
+        }))
+    }
+
+    function handleCategoryChange(event) {
+        setSelectedCategory(event.target.value);
     }
 
     return (
@@ -69,6 +123,7 @@ function ExpensesRegister() {
             <ul id="expense-list">
                 {expensesList}
             </ul>
+            {isDetailShown ? expenseDetail : ""}
         </div>
     )
 }
