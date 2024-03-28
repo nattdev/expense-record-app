8dea13d810b25341788a7ea128032eaad42c8ac6
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-27 20:12:39 -0500

    Create CurrentDate component

---
+++ client/src/components/CurrentDate.jsx
@@ -0,0 +1,24 @@
+function CurrentDate() {
+    function formatDate(dateString) {
+        const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+        const date = new Date(dateString);
+        const day = date.getDate();
+        const monthIndex = date.getMonth();
+        const year = date.getFullYear();
+
+        const formattedDate = `${day} de ${months[monthIndex]} ${year}`;
+        return formattedDate;
+    }
+
+    const currentDate = new Date().toISOString();
+    const formattedDate = formatDate(currentDate);
+
+    return (
+        <div>
+            {formattedDate}
+        </div>
+    );
+}
+
+export default CurrentDate;
