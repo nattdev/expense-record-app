6ac9bc9fbf97dee5c8ec1ff39936cd13253fc6c7
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-29 21:29:37 -0500

    Add tailwindcss styles

--- client/src/components/RemainingBudget.jsx
+++ client/src/components/RemainingBudget.jsx
@@ -4,8 +4,8 @@ function RemainingBudget() {
     const {totalExpenses, currentBudget} = useExpenses();
 
     return (
-        <div id="remaining-budget-wrapper">
-        <p>{(currentBudget - totalExpenses).toFixed(2)}</p><p>SALDO RESTANTE</p>
+        <div id="remaining-budget-wrapper" className="bg-lime-500 p-6 rounded-3xl">
+        <p className="font-bold text-3xl">{(currentBudget - totalExpenses).toFixed(2)}</p><p>SALDO RESTANTE</p>
         </div>
     )
 }
