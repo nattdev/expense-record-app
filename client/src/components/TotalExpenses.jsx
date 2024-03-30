1f8c5f50d250ce0a63bc10b19937c900e0d249cb
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-29 21:29:43 -0500

    Add tailwindcss styles

--- client/src/components/TotalExpenses.jsx
+++ client/src/components/TotalExpenses.jsx
@@ -5,8 +5,8 @@ function TotalExpenses () {
     const {totalExpenses} = useExpenses();
     
     return (
-        <div id="total-expenses-wrapper">
-            <p>{totalExpenses}</p><p>GASTOS TOTALES</p>
+        <div id="total-expenses-wrapper" className="bg-pink-700 p-6 rounded-3xl">
+            <p className="font-bold text-3xl">{totalExpenses}</p><p>GASTOS TOTALES</p>
         </div>
     )
 }
