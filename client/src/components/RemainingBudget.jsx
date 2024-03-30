73855b8f22d175bd0ebc3d745a40e08d1b675e9c
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-29 21:36:01 -0500

    Create remainingBudget state

--- client/src/components/RemainingBudget.jsx
+++ client/src/components/RemainingBudget.jsx
@@ -1,11 +1,17 @@
+import { useEffect, useState } from "react";
 import { useExpenses } from "./ExpensesContext";
 
 function RemainingBudget() {
     const {totalExpenses, currentBudget} = useExpenses();
+    const [remainingBudget, setremainingBudget] = useState();
+
+    useEffect(() => {
+        setremainingBudget((currentBudget - totalExpenses).toFixed(2));
+    }, [totalExpenses, currentBudget]);
 
     return (
         <div id="remaining-budget-wrapper" className="bg-lime-500 p-6 rounded-3xl">
-        <p className="font-bold text-3xl">{(currentBudget - totalExpenses).toFixed(2)}</p><p>SALDO RESTANTE</p>
+        <p className="font-bold text-3xl">{remainingBudget}</p><p>SALDO RESTANTE</p>
         </div>
     )
 }
