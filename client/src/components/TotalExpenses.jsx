48e66185f99833846d6059e1a265d5708f44878b
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-03-30 19:28:29 -0500

    Add responsive tailwind styles to sm devices

--- client/src/components/TotalExpenses.jsx
+++ client/src/components/TotalExpenses.jsx
@@ -6,7 +6,7 @@ function TotalExpenses () {
     
     return (
         <div id="total-expenses-wrapper" className="bg-pink-700 py-4 px-4 sm:p-6 rounded-3xl grow">
-            <p className="font-bold text-3xl">{totalExpenses}</p><p>GASTOS TOTALES</p>
+            <p className="font-bold text-2xl sm:text-3xl">{totalExpenses}</p><p>GASTOS TOTALES</p>
         </div>
     )
 }
