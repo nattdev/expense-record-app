2249072f942f2459fe0b150ae482035e2e0a37e4
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-02 20:38:38 -0500

    Change githubIcon.svg to component .jsx and add dark-mode styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,12 @@
-import githubIcon from "../assets/icons/github-icon.svg";
+import GithubIcon from "../assets/icons/GithubIcon.jsx";
 
 function Footer() {
     return (
-        <footer className="text-sm sm:text-base m-3 mt-6 font-medium flex">
-            <p>Develop and Design by nattdev</p><a target="_blank" href="https://github.com/nattdev"><img src={githubIcon} className="mx-3"></img></a>
+        <footer className="text-sm sm:text-base m-3 mt-6 font-normal flex dark:text-white">
+            <p className="mr-3">Develop and Design by nattdev</p><a target="_blank" href="https://github.com/nattdev">
+                <GithubIcon />
+            </a>
+
         </footer>
     );
 }
