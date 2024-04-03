85c6400a17b7d76d72a0134c3f9b8f465f76515e
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-02 23:16:05 -0500

    Change colors styles

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -2,7 +2,7 @@ import { Datepicker } from "flowbite-react";
 
 function Calendar() {
     return (
-        <div className="shadow-md col-span-2 text-center py-6 sm:p-6 rounded-3xl bg-white dark:bg-[black] dark:text-slate-200">
+        <div className="shadow-md col-span-2 text-center py-6 sm:p-6 rounded-3xl bg-white dark:bg-[#354250] dark:text-slate-200">
             <header>
                 <p className="text-2xl font-medium">Calendario</p>
             </header>
