7b05fca048475a88c4f38d0a7a18fb647f39bd7a
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-04-21 01:35:42 -0500

    Create categories state into a Context

--- client/src/components/ExpensesContext.jsx
+++ client/src/components/ExpensesContext.jsx
@@ -4,22 +4,23 @@ export const ExpensesContext = createContext([]);
 
 export const useExpenses = () => {
     const context = useContext(ExpensesContext);
-    if(!context) {
+    if (!context) {
         throw new Error("useExpenses must be used within a ExpensesContextProvider");
     } else {
         return context;
     }
 }
 
-export const ExpensesContextProvider = ({children}) => {
+export const ExpensesContextProvider = ({ children }) => {
     const [expenses, setExpenses] = useState([]);
     const [totalExpenses, setTotalExpenses] = useState(0);
     const [currentBudget, setCurrentBudget] = useState(0);
+    const [categories, setCategories] = useState([]);
 
     return (
-        <ExpensesContext.Provider value={{expenses, setExpenses, totalExpenses, setTotalExpenses, currentBudget, setCurrentBudget}}>
+        <ExpensesContext.Provider value={{ expenses, setExpenses, totalExpenses, setTotalExpenses, currentBudget, setCurrentBudget, categories, setCategories }}>
             {children}
         </ExpensesContext.Provider>
-    ) 
+    )
 
 }
