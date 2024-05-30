a1033108a59c19e77a9dbcb1bd00986dce15aa23
Vanessa <110750471+nattdev@users.noreply.github.com>
2024-05-29 19:04:09 -0500

    Change GithubIcon jsx file to svg file

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,11 @@
-import GithubIcon from "../assets/icons/GithubIcon.jsx";
+import GithubIcon from "../assets/icons/GithubIcon.svg";
 
 function Footer() {
     return (
         <footer className="text-sm sm:text-base m-3 mt-6 font-normal flex dark:text-white">
             <p className="mr-3">Develop and Design by nattdev</p><a target="_blank" href="https://github.com/nattdev">
-                <GithubIcon />
+                <img src={GithubIcon}></img>
             </a>
-
         </footer>
     );
 }
